6d17af926c2a12d7646a2c8ccb60259abd434947
Ivan Babushkin <ibabushkin@thumbtack.com>
2015-11-09 13:48:32 -0800

    Highlight trailing whitespace

--- init.el
+++ init.el
@@ -133,6 +133,8 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 
+(setq-default show-trailing-whitespace t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -146,3 +148,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
