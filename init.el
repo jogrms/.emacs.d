fd4035dfd35bd54759c6626258ac7b188859d00d
Ivan Babushkin <jogrms@gmail.com>
2016-01-04 00:56:10 -0800

    Add puppet mode

--- init.el
+++ init.el
@@ -71,7 +71,8 @@
     php-mode
     web-mode
     markdown-mode+
-    less-css-mode))
+    less-css-mode
+    puppet-mode))
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
