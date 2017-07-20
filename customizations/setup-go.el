cd8317fbbf62173ab33ad741a7bb3d9004aa7f9e
Ivan Babushkin <jogrms@gmail.com>
2017-07-20 12:44:12 -0700

    Add go installation to emacs exec-path

--- customizations/setup-go.el
+++ customizations/setup-go.el
@@ -1,5 +1,8 @@
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/go/bin"))
+(setq exec-path (append exec-path '("/usr/local/go/bin")))
+
 (add-hook 'go-mode-hook
           (lambda ()
-            (add-hook 'before-save-hook 'gofmt-before-save)
+            (add-hook 'before-save-hook #'gofmt-before-save)
             (setq tab-width 4)
             (setq indent-tabs-mode 1)))
