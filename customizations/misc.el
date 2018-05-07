36baae39e5c96957b97e8bd43d52a5a82ccc15cb
Ivan Babushkin <jogrms@gmail.com>
2018-05-07 13:20:15 -0700

    Add open in Chrome

--- customizations/misc.el
+++ customizations/misc.el
@@ -16,3 +16,14 @@
 (setq whitespace-line-column 100)
 (setq whitespace-style '(face lines-tail))
 (global-whitespace-mode +1)
+
+;; eg when in a text file, (execute-shell-command-on-buffer "p4 edit %s")
+;; will check out the file in perforce
+(defun execute-shell-command-on-buffer (shell-command-text)
+  (shell-command (format shell-command-text (shell-quote-argument buffer-file-name))))
+
+;; Open in Chrome
+(defun chrome-open ()
+  (interactive)
+  (execute-shell-command-on-buffer "open -a 'Google Chrome' %s"))
+
